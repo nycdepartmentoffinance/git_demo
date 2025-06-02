b01ed3f1392dedb1655f255b05b0f5782bcb22b1
Claire Boyd <BoydClaire@finance.nyc.gov>
2025-06-02 12:11:50 -0400

    first commit

---
+++ demo.R
@@ -0,0 +1,15 @@
+#import libraries
+library(dplyr)
+library(readr)
+
+# read in source data
+data <- readr::read_csv("data/quintos_sample_revised.csv")
+
+# add a new column, logged price
+output_data <- data %>%
+    mutate(
+        log_price = log(price)
+    )
+
+# export the same dataset
+write.csv(output_data, "data/demo_output_data.csv")
